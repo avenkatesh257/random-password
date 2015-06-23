365f1b91ea75254c960af02d7eae476d2c51654d
Arthy Venkatesh <Avenkatesh257@gmail.com>
2015-06-22 19:42:44 -0700

    password generator

---
+++ CodeAppetizer.js
@@ -0,0 +1,24 @@
+// Code Appetizer
+
+// Using javascript, create a random password generator. Your function should accept the length of the password as an argument and return a password with any of the chars A-Z, a-z, 0-9, !, $
+
+// Class Exercise
+
+// Exercise taken from Stoyan Stefanov's 'Onject Oriented Javascript' book 
+// 1. Create an object called 'shape' that has a 'type' property and a 'getType' method. 
+// 2. Define a Triangle constructor function whose prototype is shape. Objects created with Triangle should have three own properties: a, b, c representing the sides of a triangle. 
+// 3. Add a new method to the prototype called getPerimeter.
+
+
+function makePasswd(passwordLength){
+	var password= '';
+	var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	for (i=0; i < passwordLength, i++) {
+	}
+	 return (passwordLength)};
+	 }
+
+
+
+
+
